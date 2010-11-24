dfb307097f329daced76d35f5354ae55564a1512
Tom May <tom@gist.com>
2010-11-24 15:28:05 -0800

    Refactor all the Cassandra access code into CassandraBlobStore and delegate to it instead of having it sprayed into three different classes with accompanying imports and confusion.

--- plugins/cassandra/src/main/java/org/elasticsearch/cassandra/blobstore/CassandraImmutableBlobContainer.java
+++ plugins/cassandra/src/main/java/org/elasticsearch/cassandra/blobstore/CassandraImmutableBlobContainer.java
@@ -23,25 +23,8 @@ import org.elasticsearch.common.blobstore.BlobPath;
 import org.elasticsearch.common.blobstore.ImmutableBlobContainer;
 import org.elasticsearch.common.blobstore.support.BlobStores;
 
-import org.apache.cassandra.thrift.Column;
-import org.apache.cassandra.thrift.ColumnOrSuperColumn;
-import org.apache.cassandra.thrift.ConsistencyLevel;
-import org.apache.cassandra.thrift.Cassandra;
-import org.apache.cassandra.thrift.InvalidRequestException;
-import org.apache.cassandra.thrift.Mutation;
-import org.apache.cassandra.thrift.TimedOutException;
-import org.apache.cassandra.thrift.UnavailableException;
-
-import org.apache.thrift.TException;
-
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Tom May (tom@gist.com)
@@ -52,87 +35,11 @@ public class CassandraImmutableBlobContainer extends AbstractCassandraBlobContai
         super(path, blobStore);
     }
 
-    // InputStream is a completely shitty abstraction for something to
-    // write via thrift.  And passing a sizeInBytes along with an
-    // InputStream is a sign that it's a shitty abstraction in
-    // general.  At least we can use the sizeInBytes to allocate a
-    // ByteBuffer and copy to it then hand it to thrift.
-    @Override public void writeBlob(final String blobName, final InputStream is, final long sizeInBytes, final WriterListener listener) {
-        blobStore.executor().execute(new Runnable() {
-            @Override public void run() {
-                try {
-                    logger.debug("writeBlob {} sizeInBytes: {}", blobKey(blobName), sizeInBytes);
-                    Cassandra.Client client =
-                        CassandraClientFactory.getCassandraClient();
-                    try {
-                        writeBlob(client, blobName, is, sizeInBytes);
-                    }
-                    finally {
-                        CassandraClientFactory.closeCassandraClient(client);
-                    }
-                    listener.onCompleted();
-                } catch (Exception e) {
-                    listener.onFailure(e);
-                }
-            }
-        });
+    @Override public void writeBlob(String blobName, InputStream is, long sizeInBytes, WriterListener listener) {
+        blobStore.writeBlob(blobPath, blobName, is, sizeInBytes, listener);
     }
 
     @Override public void writeBlob(String blobName, InputStream is, long sizeInBytes) throws IOException {
         BlobStores.syncWriteBlob(this, blobName, is, sizeInBytes);
-    }
-
-    private void writeBlob(Cassandra.Client client, String blobName, InputStream is, long sizeInBytes)
-        throws InvalidRequestException, TimedOutException, UnavailableException, TException, IOException
-    {
-        long timestamp = System.currentTimeMillis();
-
-        Map<String, Map<String, List<Mutation>>> mutationMap =
-            new HashMap<String, Map<String, List<Mutation>>>();
-
-        // Insert the blob data into Blobs.
-
-        int intSizeInBytes = (int) sizeInBytes;
-        if (intSizeInBytes != sizeInBytes) {
-            throw new IllegalArgumentException(
-                "Blob " + blobName + " size " + sizeInBytes +
-                " is too large.");
-        }
-        ByteBuffer blobData = ByteBuffer.allocate(intSizeInBytes);
-        new DataInputStream(is).readFully(blobData.array());
-
-        List<Mutation> blobsMutations = new ArrayList<Mutation>();
-        blobsMutations.add(createInsert("data", blobData, timestamp));
-
-        Map<String, List<Mutation>> blobsMutationMap =
-            new HashMap<String, List<Mutation>>();
-        blobsMutationMap.put("Blobs", blobsMutations);
-
-        mutationMap.put(blobKey(blobName), blobsMutationMap);
-
-        // Insert the blobName into BlobNames.
-
-        ByteBuffer size = utf8.encode(Long.toString(sizeInBytes));
-
-        List<Mutation> blobNamesMutations = new ArrayList<Mutation>();
-        blobNamesMutations.add(createInsert(blobName, size, timestamp));
-
-        Map<String, List<Mutation>> blobNamesMutationMap =
-            new HashMap<String, List<Mutation>>();
-        blobNamesMutationMap.put("BlobNames", blobNamesMutations);
-
-        mutationMap.put(blobPath, blobNamesMutationMap);
-
-        client.batch_mutate(
-            keySpace, mutationMap, ConsistencyLevel.QUORUM);
-    }
-
-    private Mutation createInsert(String name, ByteBuffer value, long timestamp) {
-        return new Mutation().setColumn_or_supercolumn(
-            new ColumnOrSuperColumn().setColumn(
-                new Column(
-                    utf8.encode(name),
-                    value,
-                    timestamp)));
     }
 }
